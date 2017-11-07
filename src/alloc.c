ee4498d0b91872b35381adac9c4779411ea7fec7
sigustin <simon.gustin@hotmail.com>
2017-11-07 20:17:50 +0100

    Print warning when trying to free already freed pointer

--- src/alloc.c
+++ src/alloc.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 
-#include "defines.h"
 #include "alloc.h"
+#include "defines.h"
 
 void del(void* pointer)
 {
    if (pointer != NULL)
       free(pointer)
+   else
+      WARNING("Tried to free already freed pointer");
    pointer = NULL;
 }
