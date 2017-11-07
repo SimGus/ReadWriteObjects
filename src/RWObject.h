4e44599d1f69fad41a299a4c8bab73c90d7d33ff
sigustin <simon.gustin@hotmail.com>
2017-11-07 19:10:16 +0100

    First commit

---
+++ src/RWObject.h
@@ -0,0 +1,15 @@
+#ifndef _RWOBJECT_H_
+#define _RWOBJECT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "defines.h"
+
+typedef struct
+{
+   ReadWriteAttribute
+} ReadWriteObject;
+
+#endif //_RWOBJECT_H_
